2a35f42bc1530dd63537337b3c1ce756c0c2b4c0
danielaniceto <danielgustavots@hotmail.com>
2023-09-29 14:12:22 -0300

    Correcao exercicio três + teste

--- test/estrutura_sequencial/exercicio_tres_test.py
+++ test/estrutura_sequencial/exercicio_tres_test.py
@@ -4,7 +4,7 @@ from src.estrutura_sequencial.exercicio_tres import imprime_resultado
 class TestExercicioTres(unittest.TestCase):
   def test_mensagem(self):
     soma = 10
-    self.assertEqual(imprime_resultado(soma), f"A soma de N1 + N2 e de {soma}.")
+    self.assertEqual(imprime_resultado(soma), f"A soma de N1 + N2 e de {soma}")
 
 if __name__ == '__main__':
     unittest.main()
