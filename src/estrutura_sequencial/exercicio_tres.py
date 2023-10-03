33430b9a79845c79f6e84e05de6f4547d0602bc0
danielaniceto <danielgustavots@hotmail.com>
2023-10-03 08:50:30 -0300

    correcao exercicio tres

--- src/estrutura_sequencial/exercicio_tres.py
+++ src/estrutura_sequencial/exercicio_tres.py
@@ -2,12 +2,15 @@
 Faça um Programa que peça dois números e imprima a soma.
 """
 
-def imprime_resultado(soma):
-    return f"A soma de N1 + N2 e de {soma}"
+def imprime_resultado(resultado):
+    return f"A soma de N1 + N2 e de {resultado}"
 
+def soma_numero(numero_um, numero_dois):
+    return numero_um + numero_dois
+    
 def main():
-    N1 = input("Digite aqui o primeiro número: ")
-    N2 = input("Digite aqui o segundo número: ")
-    soma = N1 + N2
-    mensagem = imprime_resultado(soma)
+    numero_um = input("Digite aqui o primeiro número: ")
+    numero_dois = input("Digite aqui o segundo número: ")
+    resultado = soma_numero(numero_um, numero_dois)
+    mensagem = imprime_resultado(resultado)
     print(mensagem)
--- test/estrutura_sequencial/exercicio_tres_test.py
+++ test/estrutura_sequencial/exercicio_tres_test.py
@@ -1,11 +1,11 @@
 import unittest
-from src.estrutura_sequencial.exercicio_tres import imprime_resultado
+from src.estrutura_sequencial.exercicio_tres import imprime_resultado, soma_numero
 
 class TestExercicioTres(unittest.TestCase):
   def test_mensagem(self):
     soma = 10
     self.assertEqual(imprime_resultado(soma), f"A soma de N1 + N2 e de {soma}")
+    self.assertEqual(soma_numero(numero_um=5,numero_dois=5), soma)
 
 if __name__ == '__main__':
     unittest.main()
-    
