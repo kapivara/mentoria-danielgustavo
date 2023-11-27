2de0b8ac0d43e6c1ad2e94184f9f44cb2a9133c8
danielaniceto <danielgustavots@hotmail.com>
2023-11-27 11:58:36 -0300

    Resolução exercicio trinta e um, inicio da implementação dos testes

--- src/estrutura_sequencial/exercicio_trinta_um.py
+++ src/estrutura_sequencial/exercicio_trinta_um.py
@@ -17,23 +17,55 @@ for D ou E.
 """
 
 def main():
+    nome_aluno = str(input("Digite seu nome caro aluno: "))
     nota_um = float(input("Digite o valor da primeira nota: "))
     nota_dois = float(input("Digite o valor da segunda nota: "))
 
     nota_media = calcula_nota_media(nota_um, nota_dois)
 
-    conceito = valida_conceito_nota(nota_media)
+    valida_conceito = valida_conceito_nota(nota_media)
 
-    retorno_mensagem_situacao_aluno = retorno_mensagem_situacao_aluno(nota_media, conceito)
+    status = valida_status_aluno(nota_media)
+
+    retorno_mensagem_situacao_aluno = retorno_mensagem_status_aluno(nome_aluno, nota_media, valida_conceito, status)
+    print(retorno_mensagem_situacao_aluno)
 
 def calcula_nota_media(nota_um, nota_dois):
 
-    if not isinstance(nota_um, nota_dois, (float, int)):
+    if not isinstance(nota_um, (float, int)):
         raise Exception ("Por favor, digite um numero correto!!!")
     
-    elif nota_um or nota_dois > 0:
-        raise Exception ("Não pode haver notas menores que zero")
-
-def valida_conceito_nota():
-
+    if not isinstance(nota_dois, (float, int)):
+        raise Exception ("Por favor, digite um numero correto!!!")
     
+    elif nota_um < 0 or nota_dois < 0:
+        raise Exception ("Não pode haver notas menores que zero")
+    
+    nota_media = (nota_um + nota_dois) / 2
+
+    return nota_media
+
+def valida_conceito_nota(nota_media):
+    conceitos =        [((0, 5), "E"),
+                       ((5, 6), "D"),
+                       ((6, 7.5), "C"),
+                       ((7.5, 9), "B"),
+                       ((9, 10), "A")]
+    
+    for intervalo, conceito in conceitos:
+        inicio, fim = intervalo
+        if inicio <= nota_media < fim:
+            return conceito
+        
+def valida_status_aluno(nota_media):
+    if nota_media > 6:
+        status = "Aprovado"
+        return status
+    else:
+        status = "Reprovado"
+        return status
+        
+def retorno_mensagem_status_aluno(nome_aluno, nota_media, valida_conceito, status):
+    return (f"Ola caro aluno {nome_aluno}, sua nota média foi de {nota_media}, seu conceito ficou classificado como {valida_conceito}, e você foi {status}")
+
+main()
