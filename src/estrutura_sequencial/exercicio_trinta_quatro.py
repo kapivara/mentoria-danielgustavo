bb69b4c8fd2cf5d7ebd35e715dd0d8535d29cc05
danielaniceto <danielgustavots@hotmail.com>
2023-12-07 15:08:02 -0300

    Resolução exercicio trinta e quatro

--- src/estrutura_sequencial/exercicio_trinta_quatro.py
+++ src/estrutura_sequencial/exercicio_trinta_quatro.py
@@ -18,6 +18,8 @@ informe-a ao usuário;
 informe-as ao usuário;
 
 """
+import math
+import sys
 
 def exceptions_validations(variable_a, variable_b, variable_c):
     if not isinstance(variable_a, (float, int)):
@@ -29,15 +31,42 @@ def exceptions_validations(variable_a, variable_b, variable_c):
     elif not isinstance(variable_c, (float, int)):
         raise Exception("Digite numeros válidos por favor!!!")
     
+    else:
+        return "Tudo ok, podemos prosseguir!!!"
+    
+def is_analysis_variable_a(variable_a):
+        if variable_a == 0:
+            print("Essa equação não é do segundo grau, logo, não pode ser calculada nessa função")
+            sys.exit()
+
 def is_calculation_equation(variable_a, variable_b, variable_c):
+    delta_calculation = variable_b**2 - 4*variable_a*variable_c
+    print(delta_calculation)
+
+    if delta_calculation < 0:
+        return "Essa equação NÃO POSSUI raizes reais!!!!"
+
+    elif delta_calculation == 0:
+        delta_calculation = -variable_b/ (2*variable_a)
+        return f"Essa equação possui APENAS uma raiz real: {delta_calculation}"
+    
+    elif delta_calculation > 0:
+        delta_calculation_raiz1 = (-variable_b + math.sqrt(delta_calculation)) / (2*variable_a)
+        delta_calculation_raiz2 = (-variable_b - math.sqrt(delta_calculation)) / (2*variable_a)
+        return f"Essa equação possui DUAS raizes reais: {delta_calculation_raiz1} e {delta_calculation_raiz2}"
 
 def main():
     variable_a = float(input("Digite o valor de A: "))
+    analysis_variables = is_analysis_variable_a(variable_a)
+
     variable_b = float(input("Digite o valor de B: "))
     variable_c = float(input("Digite o valor de C: "))
 
-    valations_variables = exceptions_validations(variable_a, variable_b, variable_c)
+    validations_exeptions_variables = exceptions_validations(variable_a, variable_b, variable_c)
 
     result_equation = is_calculation_equation(variable_a, variable_b, variable_c)
+
+    print(validations_exeptions_variables)
+    print(result_equation)
 
 main()
